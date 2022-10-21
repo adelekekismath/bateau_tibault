4d3801b3ce4dad9e29fdfadac58cf34823edd3ae
Nour94276 <tounsinourhouda@gmail.com>
2022-10-22 00:51:41 +0200

    finish fonctionnel static part

--- ReacteNative/screens/HomeScreen.tsx
+++ ReacteNative/screens/HomeScreen.tsx
@@ -5,37 +5,47 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function HomeScreen ({ navigation }: RootTabScreenProps<'Home'>) {
-  const customData = require('../data/boats.json');
+  const customDataRestaurant = require('../data/restaurants.json');
   const customDataBoat = require('../data/boats.json');
+  const customDataRecette = require('../data/recettes.json');
+  
   let onPress: ()=>void ; 
   let databoat = {};
-    const affichecomponent = (_Object: any)=>{
-      if (_Object == "Restaurant")
+    const affichecomponent = (element : string)=>{
+      let _objectif = "List";
+      if (element == "restaurant")
       {
-        const result = JSON.stringify(customData)
+        const result = JSON.stringify(customDataRestaurant)
         databoat = JSON.parse(result);
-        navigation.navigate(_Object, databoat)
-        console.log(databoat)
       }
-      if (_Object == "List")
+      if (element == "boat")
       {
-        
         const result = JSON.stringify(customDataBoat)
         databoat = JSON.parse(result);
-        navigation.navigate(_Object, databoat)
-        console.log(databoat)
       }
+      if (element == "recette")
+      {
+        const result = JSON.stringify(customDataRecette)
+        databoat = JSON.parse(result);
+      }
+      navigation.navigate(_objectif, databoat)
     };
 
 
   return (
    
     <View style={styles.container}>
-      <TouchableOpacity onPress={() =>affichecomponent("Restaurant")}> 
+      <TouchableOpacity onPress={() =>affichecomponent("restaurant")}> 
       <Button variant="primary">Restaurant</Button>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() =>affichecomponent("List")}> 
-      <Button variant="primary">Boat</Button>
+      <TouchableOpacity onPress={() =>affichecomponent("boat")}> 
+      <Button variant="primary">Bateau</Button>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() =>affichecomponent("recette")}> 
+      <Button variant="primary">Recette</Button>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() =>affichecomponent("boat")}> 
+      <Button variant="primary">Contact</Button>
       </TouchableOpacity>
     </View>
   );
--- ReacteNative/screens/ListScreen.tsx
+++ ReacteNative/screens/ListScreen.tsx
@@ -2,22 +2,73 @@ import { useRoute } from "@react-navigation/native";
 import React from "react";
 import { View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
-import { StyleSheet, TouchableOpacity } from 'react-native';
-import Button from 'react-bootstrap/Button';
+import { SafeAreaView, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+
+import { Card, FAB, Button, Title } from "react-native-paper";
+import { white } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
+
 
 
 export default function ListScreen ({ navigation }: RootTabScreenProps<'List'>) {
     const route = useRoute(); 
     const savelementinformation= (_Object: any)=>{return navigation.navigate("SingleElement" ,_Object) };
     return (
-        route.params.map((userData) => {
+       console.log("Hi"),
+       <SafeAreaView   style={styles.container} >
+       <ScrollView style={styles.contentContainer} horizontal={true} >
+
+       {route.params.map((userData) => {
             return  (
-                <TouchableOpacity onPress={() =>savelementinformation(userData)} > 
-              <Button variant="primary">{userData.name}</Button>
+                <TouchableOpacity style={styles.contentContainer} onPress={() =>savelementinformation(userData)} >
+     
+                 <Card style={styles.Card}> <Button icon="plus" > </Button>
+    <Card.Title title={userData.name} />
+             {/* <Card.Content>
+      <Title>{userData.name}</Title>
+    </Card.Content> */}
+    <Card.Cover source={{ uri: userData.image }}/>
+  </Card>
               </TouchableOpacity>
-              );
-        })
-    );
-   
+              
+       )})
+            }
+        </ScrollView>
+        </SafeAreaView>
       
+    );
       }
+
+      const styles = StyleSheet.create({
+        fab: {
+            position: 'absolute',
+            margin: 16,
+            right: 0,
+            bottom: 0,
+            color:'white'
+          },
+        container: {
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        title: {
+          fontSize: 20,
+          fontWeight: 'bold',
+        },
+        Card : {
+        
+        width : '80%',
+         marginRight : '-20%'
+        },
+        contentContainer: {
+            height: 0.8,
+            width: '90%',
+        
+        },
+        scrollView: {
+            marginHorizontal: 20,
+            paddingVertical: 20 , 
+            paddingHorizontal : 20 
+          },
+      });
+      
